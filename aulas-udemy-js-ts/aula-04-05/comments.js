e8fda11b69db804aa2bb66792985f3445f9b314e
Mateus Inácio <mateusinaciosilveira46@yahoo.com>
2026-08-11 11:07:35 -0300

    aprendizado de node no nagevador e no terminal do vsc

---
+++ aulas-udemy-js-ts/aula-04-05/comments.js
@@ -0,0 +1,9 @@
+/*
+para escrever um comentário de bloco se usa no começo e no final do bloco de comentário
+*/  
+
+console.log('Olá, mundo!'); // comentário de linha
+console.log('Linha 01');
+console.log('Linha 02');
+console.log('Linha 03');
+console.log('Linha 04');
---
+++ aulas-udemy-js-ts/aula-04-05/index.js
@@ -0,0 +1,3 @@
+// Uma forma de mesclar HTML com JS é utilizando o document.write()
+console.log("Olá, mundo!");
+console.log('Este trecho será exibido no console do navegador');
